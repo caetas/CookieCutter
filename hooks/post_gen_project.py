4815ad70774e064bae0ec61381e1f172dd1307ba
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2025-09-17 10:14:57 +0200

    test

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -53,6 +53,8 @@ def remove_temp_folders(temp_folders) -> None:
 
 
 def remove_cli_script(cli_enable: str, project_slug: str) -> None:
+    print(cli_enable)
+    print(os.listdir(f"src/{project_slug}"))
     if cli_enable == "none":
         os.remove(f"src/{project_slug}/cli.py")
 
